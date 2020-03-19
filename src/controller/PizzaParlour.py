5be73a5773def91a883c9cc5f8afbc1c231fe3a1
KarenChen <kareny.chen@mail.utoronto.ca>
2020-03-19 15:28:55 -0400

    feature5: implemented & ready for testing

--- src/controller/PizzaParlour.py
+++ src/controller/PizzaParlour.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request, abort
+import os
+import sys
+sys.path.append("../model")
+
+from Menu import Menu
+from Order import Order
 
 app = Flask("Assignment 2")
+
+# global variables
+orders = []
 
 @app.route('/pizza')
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
 
+@app.route('/menu/full')
+def get_full_menu():
+    return jsonify(Menu().get_full_menu())
+
+@app.route('/menu/search-by-name/<name>')
+def get_price_by_name(name):
+    return jsonify([Menu().get_price_by_name(name)])
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='localhost', debug=True, port=os.environ.get('PORT', 8848))
