5bf7c7f77109dd3a41afb5e64242b81e0d897447
KarenChen <kareny.chen@mail.utoronto.ca>
2020-03-19 16:02:27 -0400

    feature1: implemented & ready for testing

--- src/controller/PizzaParlour.py
+++ src/controller/PizzaParlour.py
@@ -15,13 +15,24 @@ orders = []
 def welcome_pizza():
     return 'Welcome to Pizza Planet!'
 
+# this method returns the full menu in the pizza parlour
 @app.route('/menu/full')
 def get_full_menu():
     return jsonify(Menu().get_full_menu())
 
+# this method searches and returns the price of an item by name
 @app.route('/menu/search-by-name/<name>')
 def get_price_by_name(name):
     return jsonify([Menu().get_price_by_name(name)])
+
+# request.json is a list of strings and lists representing items in the order
+# this method returns the order number and the total price of this order
+@app.route('/new-order', methods=['POST'])
+def new_order():
+    items = request.json
+    order = Order(items)
+    orders.append(order)
+    return jsonify([order.order_num, order.price])
 
 if __name__ == "__main__":
     app.run(host='localhost', debug=True, port=os.environ.get('PORT', 8848))
