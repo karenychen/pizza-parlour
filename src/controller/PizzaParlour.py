86910dec96c07c6c6902ecaadf7e725c26ad9888
kexinlin <kexin.lin@mail.utoronto.ca>
2020-04-04 00:58:01 +0800

    feature 4: change delivery from list to class object, add DeliveryFactory

--- src/controller/PizzaParlour.py
+++ src/controller/PizzaParlour.py
@@ -7,21 +7,22 @@ import csv
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 model_folder = os.path.join(THIS_FOLDER, '../model')
 sys.path.append(model_folder)
-foodora_file = os.path.join(THIS_FOLDER, './../../data/foodora.csv')
 
 from Menu import Menu
 from Order import Order
+from Delivery import DeliveryFactory, Delivery, InHouseDelivery, UberEatsDelivery, FoodoraDelivery
 
 app = Flask("Assignment 2")
 
 # === global variables ===
 menu_service = Menu()
-orders = []  # List[Union[List[str, str, Dict[str, int]], str]]
+delivery_factory = DeliveryFactory()
+orders = []  # List[Order]
 
 pickup_orders = []  # List[int], list of order numbers
-in_house_delivery = []  # list of list with order number, order details, address as the three elements inside
-uber_eats_delivery = []  # list of list with order number, order details, address as the three elements inside
-foodora_delivery = []  # list of list with order number, order details, address as the three elements inside
+in_house_delivery = []  # List[InHouseDelivery]
+uber_eats_delivery = []  # List[UberEatsDelivery]
+foodora_delivery = []  # List[FoodoraDelivery]
 
 # === helper functions ===
 # this function check if an item is valid, an item is either a pizza or a drink
@@ -131,79 +132,70 @@ def add_pickup():
 # Example of request.json: [56, "Call me when arriving the lobby", "253 College Street"]
 @app.route('/add-in-house-delivery', methods=['POST'])
 def add_in_house_delivery():
-    if len(request.json) != 3:
-        abort(400, 'Wrong number of arguments, expected 3.')
-    elif not isinstance(request.json[0], int):
-        abort(400, 'Invalid order number.')
-    elif not isinstance(request.json[1], str):
-        abort(400, 'Invalid order details.')
-    elif not isinstance(request.json[2], str):
-        abort(400, 'Invalid address.')
+    create_delivery_result = DeliveryFactory(0, request.json) # 0 means in-house delivery
+    if(create_delivery_result[0]): # if the first return value is not 0, then it is an error code
+        abort(create_delivery_result[0], create_delivery_result[1]) # in this case, the second is the error message
+    else:
+        # if the first return value is 0, then the second is the created Delivery object
+        new_delivery = create_delivery_result[1]
     
     # check if the order has already been added to the in-house delivery list.
     for delivery in in_house_delivery:
-        if delivery[0] == request.json[0]:
+        if delivery.order_num == new_delivery.order_num:
             abort(400, "The order has already been added for in-house delivery.")
 
     # check if the order number exists.
     for order in orders:
-        if order.order_num == request.json[0]:
-            new_delivery = [request.json[0], request.json[1], request.json[2]]
+        if order.order_num == new_delivery.order_num:
             in_house_delivery.append(new_delivery)
-            return jsonify(request.json[0])
+            return jsonify(new_delivery.order_num)
     abort(404, "The order number for in-house delivery does not exist. ")
 
 # request.json is a list of 3 element: [order_number: int, order_details: str, address: str]
 # Example of request.json: [56, "Call me when arriving the lobby", "253 College Street"]
 @app.route('/add-uber-eats', methods=['POST'])
 def add_uber_eats():
-    if len(request.json) != 3:
-        abort(400, 'Wrong number of arguments, expected 3.')
-    elif not isinstance(request.json[0], int):
-        abort(400, 'Invalid order number.')
-    elif not isinstance(request.json[1], str):
-        abort(400, 'Invalid order details.')
-    elif not isinstance(request.json[2], str):
-        abort(400, 'Invalid address.')
-
+    create_delivery_result = DeliveryFactory(1, request.json) # 1 means Uber Eats delivery
+    if(create_delivery_result[0]): # if the first return value is not 0, then it is an error code
+        abort(create_delivery_result[0], create_delivery_result[1]) # in this case, the second is the error message
+    else:
+        # if the first return value is 0, then the second is the created Delivery object
+        new_delivery = create_delivery_result[1]
+    
     # check if the order has already been added to the in-house delivery list.
     for delivery in uber_eats_delivery:
-        if delivery[0] == request.json[0]:
+        if delivery.order_num == new_delivery.order_num:
             abort(400, "The order has already been added for Uber Eats delivery.")
-    
+
     # check if the order number exists.
     for order in orders:
-        if order.order_num == request.json[0]:
-            new_uber_eats = [request.json[0], request.json[1], request.json[2]]
-            uber_eats_delivery.append(new_uber_eats)
-            return jsonify(request.json[0])
+        if order.order_num == new_delivery.order_num:
+            uber_eats_delivery.append(new_delivery)
+            return jsonify(new_delivery.order_num)
     abort(404, "The order number for Uber Eats delivery does not exist. ")
+    
 
 # Read the first line in foodora.csv, and add that order information into foodora_delivery list after checking
 # if the order has been added yet.
 @app.route('/add-foodora')
 def add_foodora():
-    with open(foodora_file, 'r') as f:
-        reader = csv.reader(f)
-        result = list(reader)[0]
+    create_delivery_result = DeliveryFactory(2, request.json) # 2 means Foodora delivery
+    if(create_delivery_result[0]): # if the first return value is not 0, then it is an error code
+        abort(create_delivery_result[0], create_delivery_result[1]) # in this case, the second is the error message
+    else:
+        # if the first return value is 0, then the second is the created Delivery object
+        new_delivery = create_delivery_result[1]
     
-    if len(result) != 3:
-        abort(400, 'Wrong number of arguments in the first line of the csv file, expected 3.')
-    elif not result[0].isdigit():
-        abort(400, 'Invalid order number.')
-    
-    order_num = result[0]
-    # check if the order has already been added to the Foodora delivery list.
+    # check if the order has already been added to the in-house delivery list.
     for delivery in foodora_delivery:
-        if delivery[0] == order_num:
-            abort(400, "The order has already been added for in-house delivery.")
+        if delivery.order_num == new_delivery.order_num:
+            abort(400, "The order has already been added for Foodora delivery.")
 
     # check if the order number exists.
     for order in orders:
-        if order.order_num == order_num:
-            new_foodora = [order_num, request.json[1], request.json[2]]
-            uber_eats_delivery.append(new_foodora)
-            return jsonify(order_num)
+        if order.order_num == new_delivery.order_num:
+            foodora_delivery.append(new_delivery)
+            return jsonify(new_delivery.order_num)
     abort(404, "The order number for Foodora delivery does not exist. ")
 
 
---
+++ src/model/Delivery.py
@@ -0,0 +1,85 @@
+from typing import List, Dict, Union, Optional
+import os
+import sys
+import csv
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+foodora_file = os.path.join(THIS_FOLDER, './../../data/foodora.csv')
+
+class DeliveryFactory:
+    '''A factory to generate object of Delivery class based on given type.'''
+    def create_valid_delivery(self, delivery_type: int, input: list):
+        '''Check whether the input is invalid. If so, initializes a new Delivery in the pizza parlour 
+        and return (0, Delivery), else return (error_code, error_message).'''
+        if(delivery_type == 0):  # type 0 means an in-house delivery
+            # check if the input is valid before initiating an in-house delivery
+            if len(input) != 3:
+                return (400, 'Wrong number of arguments, expected 3.')
+            elif not isinstance(input[0], int):
+                return (400, 'Invalid order number.')
+            elif not isinstance(input[1], str):
+                return (400, 'Invalid order details.')
+            elif not isinstance(input[2], str):
+                return (400, 'Invalid address.')
+            return (0, InHouseDelivery(input[0], input[1], input[2]))
+
+        if(delivery_type == 1):  # type 1 means a Uber Eats delivery
+            # check if the input is valid before initiating a Uber Eats delivery
+            if len(input) != 3:
+                return (400, 'Wrong number of arguments, expected 3.')
+            elif not isinstance(input[0], int):
+                return (400, 'Invalid order number.')
+            elif not isinstance(input[1], str):
+                return (400, 'Invalid order details.')
+            elif not isinstance(input[2], str):
+                return (400, 'Invalid address.')
+            return (0, UberEatsDelivery(input[0], input[1], input[2]))
+
+        if(delivery_type == 2):  # type 2 means a Foodora delivery
+            # check if the input is valid before initiating a Foodora delivery
+            with open(foodora_file, 'r') as f:
+                reader = csv.reader(f)
+                result = list(reader)[0]
+            if len(result) != 3:
+                return (400, 'Wrong number of arguments in the first line of the csv file, expected 3.')
+            elif not result[0].isdigit():
+                return (400, 'Invalid order number.')
+            return (0, FoodoraDelivery(input[0], input[1], input[2]))
+
+
+class Delivery:
+    '''A food delivery request for orders in the pizza parlour.
+    
+    === Attributes ===
+    order_num: the number of the order requested for delivery
+    order_details: the details added for the order requested for delivery
+    address: the address for the the order requested for delivery
+    '''
+
+    order_num: int
+    order_details: str
+    address: str
+
+    def __init__(self, order_num, order_details, address) -> None:
+        self.order_num = order_num
+        self.order_details = order_details
+        self.address = address
+
+    
+
+class InHouseDelivery(Delivery):
+    '''An in-house delivery request for orders in the pizza parlour.
+    Class leave for further extension.'''
+    pass
+
+
+class UberEatsDelivery(Delivery):
+    '''A Uber Eats delivery request for orders in the pizza parlour.
+    Class leave for further extension.'''
+    pass
+
+
+class FoodoraDelivery(Delivery):
+    '''A Foodora delivery request for orders in the pizza parlour.
+    Class leave for further extension.'''
+    pass
